612b7403bd9b86923e8c94d717eb0cb2c59809a7
dciancu <daniel@iancu.io>
2024-05-23 17:36:03 +0300

    Fixed publish script

--- publish.sh
+++ publish.sh
@@ -8,7 +8,8 @@ cd "$SCRIPT_DIR"
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'test' ]]; then
-    docker push "${DOCKERHUB_IMAGE}:test-stable" "${DOCKERHUB_IMAGE}:test-edge"
+    docker push "${DOCKERHUB_IMAGE}:test-stable"
+    docker push "${DOCKERHUB_IMAGE}:test-edge"
 else
     docker push --all-tags "$DOCKERHUB_IMAGE"
 fi

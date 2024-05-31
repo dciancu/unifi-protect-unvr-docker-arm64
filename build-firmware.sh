4dfd8789b7e2c9d7d3514def3c22c205db534d00
dciancu <daniel@iancu.io>
2024-06-01 00:37:00 +0300

    Added --pull option to docker build commands
    This helps with base image cache handling.

--- build-firmware.sh
+++ build-firmware.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
-docker build -f firmware-base.Dockerfile -t unvr-firmware-base .
+docker build -f firmware-base.Dockerfile -t unvr-firmware-base --pull .
 docker build -f firmware.Dockerfile --no-cache -t unvr-firmware .
 if [ -f firmware/version ]; then
     rm -r firmware/*
--- build-protect.sh
+++ build-protect.sh
@@ -8,11 +8,11 @@ cd "$SCRIPT_DIR"
 image_name="${DOCKERHUB_IMAGE:-unvr}"
 
 if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'test' ]]; then
-    docker build -f protect.Dockerfile --build-arg UNVR_STABLE=1 -t "${image_name}:test-stable" .
+    docker build -f protect.Dockerfile --build-arg UNVR_STABLE=1 -t "${image_name}:test-stable" --pull .
     docker build -f protect.Dockerfile -t "${image_name}:test-edge" .
 else
     version="$(tr -d '\n' < firmware/version)"
-    docker build -f protect.Dockerfile --build-arg UNVR_STABLE=1 -t "${image_name}:stable" .
+    docker build -f protect.Dockerfile --build-arg UNVR_STABLE=1 -t "${image_name}:stable" --pull .
     docker tag "${image_name}:stable" "${image_name}:${version}"
     docker build -f protect.Dockerfile -t "${image_name}:edge" .
 fi

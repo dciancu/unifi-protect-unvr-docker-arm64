582c97bb18b54e67d5fde8067d9627f6d0f84d40
dciancu <daniel@iancu.io>
2024-06-01 00:23:13 +0300

    Minor build-firmware script improvement

--- build-firmware.sh
+++ build-firmware.sh
@@ -7,5 +7,7 @@ cd "$SCRIPT_DIR"
 
 docker build -f firmware-base.Dockerfile -t unvr-firmware-base .
 docker build -f firmware.Dockerfile --no-cache -t unvr-firmware .
-rm -rf firmware/* || true
+if [ -f firmware/version ]; then
+    rm -r firmware/*
+fi
 docker build -f firmware-copy.Dockerfile --output firmware .

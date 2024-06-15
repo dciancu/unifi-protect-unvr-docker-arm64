0b7f7909d9e3a4923fc96a02ddb17a03e5846695
dciancu <daniel@iancu.io>
2024-06-15 15:40:00 +0300

    Added ALL_DEBS build arg to firmware build

--- build-firmware.sh
+++ build-firmware.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
 docker build -f firmware-base.Dockerfile -t unvr-firmware-base --pull .
-docker build -f firmware.Dockerfile --no-cache -t unvr-firmware --build-arg "FW_URL=${FW_URL:-}" .
+docker build -f firmware.Dockerfile --no-cache -t unvr-firmware \
+    --build-arg "FW_URL=${FW_URL:-}" --build-arg "ALL_DEBS=${ALL_DEBS:-}" .
 if [ -f firmware/version ]; then
     rm -r firmware/*
 fi

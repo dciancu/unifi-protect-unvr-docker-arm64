148381bbbcaeb0365aece891307a9a4df77fe767
dciancu <daniel@iancu.io>
2024-06-25 00:08:25 +0300

    Better handling of build context

--- build-firmware.sh
+++ build-firmware.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
-docker build -f firmware-base.Dockerfile -t unvr-firmware-base --pull .
-docker build -f firmware.Dockerfile --no-cache -t unvr-firmware \
-    --build-arg "FW_URL=${FW_URL:-}" --build-arg "ALL_DEBS=${ALL_DEBS:-}" .
+docker build -t unvr-firmware-base --pull - < firmware-base.Dockerfile
+docker build --no-cache -t unvr-firmware \
+    --build-arg "FW_URL=${FW_URL:-}" --build-arg "ALL_DEBS=${ALL_DEBS:-}" - < firmware.Dockerfile
 if [ -f firmware/version ]; then
     rm -r firmware/*
 fi
-docker build -f firmware-copy.Dockerfile --output firmware .
+docker build --output firmware - < firmware-copy.Dockerfile

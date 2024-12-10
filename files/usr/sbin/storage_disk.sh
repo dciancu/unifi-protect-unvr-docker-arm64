f7f6baa6933792fcba05442b80ec1331ccb7660f
dciancu <daniel@iancu.io>
2024-12-10 22:36:55 +0200

    Minor fixes

--- files/usr/sbin/storage_disk.sh
+++ files/usr/sbin/storage_disk.sh
@@ -6,6 +6,7 @@ done
 
 debug="${DEBUG:-false}"
 disk="${STORAGE_DISK:-/dev/sda1}"
+echo '#!/bin/bash' > /etc/default/storage_disk
 echo "DEBUG=${debug}" > /etc/default/storage_disk
 echo "STORAGE_DISK=${disk}" >> /etc/default/storage_disk
 echo 'source /usr/sbin/storage_disk_debug.sh' >> /etc/default/storage_disk

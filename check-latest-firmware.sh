f1143bc8c1417d7b98b26bd726cb847756cee3b8
dciancu <daniel@iancu.io>
2025-01-17 23:25:34 +0200

    Improved check-latest-firmware.sh

--- check-latest-firmware.sh
+++ check-latest-firmware.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
@@ -13,9 +16,16 @@ echo "Latest Firmware: ${LATEST_FIRMWARE}"
 echo "Latest Repo Firmware: ${LATEST_REPO_FIRMWARE}"
 
 if [[ "$LATEST_FIRMWARE" == "$LATEST_REPO_FIRMWARE" ]]; then
-    echo 'Latest firmware in repo ok.'
+    echo -e "${GREEN}Latest firmware in repo ok.${NC}"
     exit 0
 else
-    echo 'Found newer latest firmware!'
+    echo -e "${RED}Found newer latest firmware!${NC}"
+    LATEST_STABLE_FIRMWARE="$(wget -q --output-document - "$FW_UPDATE_URL" \
+        | jq -r '._embedded.firmware | map(select(.probability_computed == 1))[0] | ._links.data.href')"
+    if [[ "$LATEST_STABLE_FIRMWARE" != "$LATEST_FIRMWARE" ]]; then
+        echo -e "${RED}WARN: Latest firmware is not marked as stable!${NC}"
+    else
+        echo -e "${GREEN}Latest firmware is marked as stable.${NC}"
+    fi
     exit 1
 fi
